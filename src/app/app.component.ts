8fa7c505fae5745cae09c51a6a4c86580c9a1908
Chagas Filho <106563976+chagasfilho007@users.noreply.github.com>
2024-03-21 20:33:58 -0300

    landing page

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'landing-page';
+}
